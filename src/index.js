55d17aad15af679fea15dd785bf8f8904d8f9996
yosefHesham <yousefmobdev@gmail.com>
2022-05-05 14:51:50 +0200

    setup-project

---
+++ src/index.js
@@ -0,0 +1,19 @@
+import ".style.css"
+
+const todos = [ {
+  description:"study linked list",
+  completed:true,
+  index : 0,
+  
+},
+{
+  description:"implement linked list with js",
+  completed:false,
+  index : 1,
+},
+{
+  description:"study stack and queue",
+  completed:false,
+  index : 2,
+}
+]
