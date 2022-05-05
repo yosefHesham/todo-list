41bcfb4640d255efb07e68cf169310e159695ed8
yosefHesham <yousefmobdev@gmail.com>
2022-05-05 16:09:12 +0200

    create headr

--- src/index.js
+++ src/index.js
@@ -22,7 +22,8 @@ const renderItems = () => {
   for(let i = 0; i < todos.length; i+=1) {
     const item = todos[i];
     const itemTemp = document.createElement("itemTemp");
-    itemTemp.innerHTML = `<div class="todo-item">
+    itemTemp.innerHTML = `<div class="item-wrapper">
+    <div class="todo-item">
     <input type="checkbox" id="vehicle1" name="vehicle1" value="Bike" />
 
     <p class="todo-title">${item.description}</p>
@@ -38,7 +39,12 @@ const renderItems = () => {
         d="M9.5 13a1.5 1.5 0 1 1-3 0 1.5 1.5 0 0 1 3 0zm0-5a1.5 1.5 0 1 1-3 0 1.5 1.5 0 0 1 3 0zm0-5a1.5 1.5 0 1 1-3 0 1.5 1.5 0 0 1 3 0z"
       />
     </svg>
-  </div>`;
+  </div>
+  ${todos.length - 1 === item.index ? `` : `<hr>`}
+  </div>
+  `;
+  console.log(itemTemp)
+  console.log(itemTemp.firstChild)
   todolist.appendChild(itemTemp.firstChild)
   
   }
