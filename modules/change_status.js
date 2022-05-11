b6989cfed99dc2218b1ec957215ed7abd1de8c43
yosefHesham <yousefmobdev@gmail.com>
2022-05-11 17:22:25 +0200

    Add checkbox eventlistner while creating new item

--- modules/change_status.js
+++ modules/change_status.js
@@ -18,19 +18,25 @@ const addCheckBoxListeners = () => {
   });
 };
 
+/**
+ *
+ * @param {Event} event
+ */
+export const changeStatus = (event) => {
+  console.log("changing status")
+  if (event.target.checked) {
+    event.target.nextElementSibling.style.textDecoration = 'line-through';
+    event.target.nextElementSibling.style.color = 'gray';
+  } else {
+    event.target.nextElementSibling.style.textDecoration = 'none';
+    event.target.nextElementSibling.style.color = 'black';
+  }
+  ToDoController.changeStatus(Number(event.target.classList[1]));
+}
 const refreshCheckBoxListeners = () => {
   const checkBoxes = document.querySelectorAll('.item-check');
   checkBoxes.forEach((checkBox) => {
-    checkBox.addEventListener('click', (event) => {
-      if (event.target.checked) {
-        event.target.nextElementSibling.style.textDecoration = 'line-through';
-        event.target.nextElementSibling.style.color = 'gray';
-      } else {
-        event.target.nextElementSibling.style.textDecoration = 'none';
-        event.target.nextElementSibling.style.color = 'black';
-      }
-      ToDoController.changeStatus(Number(event.target.classList[1]));
-    });
+    checkBox.addEventListener('click', changeStatus)
   });
 };
 
